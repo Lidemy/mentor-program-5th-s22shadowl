a26f4ce7b3512fd9456a4a63ad67dfea95c8b91d
s22shadowl <jjerry409@gmail.com>
2021-04-25 23:30:01 +0800

    0425

--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,10 +1,20 @@
 function join(arr, concatStr) {
-  
+    let result = '';
+    for( let i = 0 ; i < arr.length ;i++ ){
+        result += arr[i] + concatStr;
+    }
+    return result;
 }
 
 function repeat(str, times) {
-  
+    let result2 = '';
+    for( let j = 0 ; j < times ; j++ ){
+        result2 += str;
+    }
+  return result2;
 }
 
 console.log(join(['a'], '!'));
 console.log(repeat('a', 5));
+
+
