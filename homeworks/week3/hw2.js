c2f39259e7c3cf9d608ff163bad2e2c9ebeda925
s22shadowl <jjerry409@gmail.com>
2021-05-02 22:29:06 +0800

    week3

--- homeworks/week3/hw2.js
+++ homeworks/week3/hw2.js
@@ -1,15 +1,15 @@
 function solve(lines) {
-  const line = lines[0].split(' ')
-  const min = Number(line[0])
-  const max = Number(line[1])
+  const maxAndmin = lines[0].split(' ')
+  const min = Number(maxAndmin[0])
+  const max = Number(maxAndmin[1])
   for (let i = min; i <= max; i++) {
-    let check = i
-    check += ''
-    let result = 0
-    for (let j = 0; j < check.length; j++) {
-      result += Math.pow(check[j], check.length)
+    let checkNum = i
+    checkNum += ''
+    let checkSum = 0
+    for (let j = 0; j < checkNum.length; j++) {
+      checkSum += Math.pow(checkNum[j], checkNum.length)
     }
-    if (i === result) {
+    if (i === checkSum) {
       console.log(i)
     }
   }
--- homeworks/week3/hw3.js
+++ homeworks/week3/hw3.js
@@ -1,16 +1,16 @@
 function solve(lines) {
   const times = lines[0]
-  let num = 0
-  let res = 0
+  let checkNum = 0
+  let checkSum = 0
   for (let i = 1; i <= times; i++) {
-    num = Number(lines[i])
-    res = 0
-    for (let j = 2; j <= num; j++) {
-      if (num % j === 0) {
-        res += j
+    checkNum = Number(lines[i])
+    checkSum = 0
+    for (let j = 2; j <= checkNum; j++) {
+      if (checkNum % j === 0) {
+        checkSum += j
       }
     }
-    if (res === num) {
+    if (checkSum === checkNum) {
       console.log('Prime')
     } else {
       console.log('Composite')
