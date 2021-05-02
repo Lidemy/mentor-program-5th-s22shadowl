ba507c8a5dc351fc942b4f3ccad9527db6b76fb9
s22shadowl <jjerry409@gmail.com>
2021-05-02 22:34:53 +0800

    week3eslint

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,11 +1,11 @@
 function reverse(str) {
-  let revStr = str.split("");
-  let newStr = [];
-  for(let i = revStr.length ; i >= 0 ; i--){  
-    newStr.push(revStr[i]);
+  const revStr = str.split('')
+  let newStr = []
+  for (let i = revStr.length; i >= 0; i--) {
+    newStr.push(revStr[i])
   }
-  newStr = newStr.join('');
-  console.log(newStr);
+  newStr = newStr.join('')
+  console.log(newStr)
 }
 
-reverse('hello');
+reverse('hello')
